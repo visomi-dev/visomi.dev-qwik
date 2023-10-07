b09667e765131ad8cf13080c369348f7fbb554e4
Michael Villalba Sotelo <visomi.dev@gmail.com>
2023-10-07 14:08:44 -0600

    fix: add no overflow on print

--- src/routes/resume/index.tsx
+++ src/routes/resume/index.tsx
@@ -4,7 +4,7 @@ import { Me } from '~/components/resume/me';
 
 export default component$(() => {
   return (
-    <div class="mx-auto h-full w-full overflow-y-auto md:h-[30cm] md:w-[21cm]">
+    <div class="no-scroll-print mx-auto h-full w-full overflow-y-auto md:h-[30cm] md:w-[21cm]">
       <Me />
     </div>
   );
